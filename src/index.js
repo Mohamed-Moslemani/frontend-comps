52baa1f98e1f6e6970f00357b45dbceffd6f6f6d
YoussefMoussallem <youssefamoussallem@gmail.com>
2026-03-16 11:36:56 +0200

    Removed HeaderNav and HeaderAction

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './styles.css';
 
 // Components
 export { default as Header } from './components/header/Header.jsx';
-export { default as HeaderActions } from './components/header/HeaderActions.jsx';
-export { default as HeaderNav } from './components/header/HeaderNav.jsx';
 export { default as Messages } from './components/chat/Messages.jsx';
 export { default as Message } from './components/chat/Message.jsx';
 export { default as InputBar } from './components/chat/InputBar.jsx';
@@ -14,6 +12,7 @@ export { default as NewChatButton } from './components/common/NewChatButton.jsx'
 export { default as ThemeToggleButton } from './components/common/ThemeToggleButton.jsx';
 export { default as CopyButton } from './components/common/CopyButton.jsx';
 export { default as DownloadButton } from './components/common/DownloadButton.jsx';
+export { default as LogoutButton } from './components/common/LogoutButton.jsx';
 
 // Authentication
 export { default as AuthGate } from './components/authentication/AuthGate.jsx';
